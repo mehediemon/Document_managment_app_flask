73888a60f88fb21ad797b3ed609a3877c7d68cdc
Mehedi <mehedi4628@gmail.com>
2024-09-01 03:22:01 +0600

    created_db script updated

--- create_db.py
+++ create_db.py
@@ -1,5 +1,16 @@
+import os
 from app import db, app
 from app.models import User, Folder, File
+
+# Define the path for the Database folder
+database_folder = os.path.join(os.path.dirname(__file__), 'Database')
+
+# Check if the Database folder exists; if not, create it
+if not os.path.exists(database_folder):
+    os.makedirs(database_folder)
+
+# Define the path for the SQLite database
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(database_folder, "site.db")}'
 
 with app.app_context():
     db.create_all()
